88eb766fd9690edf9b73a88e2fc9b21ddfbca367
Tim Weißenfels <timw-public@outlook.com>
2020-08-18 14:24:17 +0200

    Read config from JSON file

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from radio.player import Player
 from radio.library import ClipLibrary
+import json
+import os
 
+# defaults
+lib_path = "test_library"
+cfg_path = "config.json"
+
+# user config
+if os.path.exists(cfg_path):
+    with open(cfg_path, "r") as config_file:
+        cfg = json.loads(config_file.read())
+        print("Using config file", config_file.name)
+        if "library" in cfg:
+            lib_path = cfg["library"]
+else:
+    print("Warning:", cfg_path, "not found.")
+
+# start radio
 print("Radio starting...")
-library = ClipLibrary("test_library")
+library = ClipLibrary(lib_path)
 player = Player(library)
 player.start()
