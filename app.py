e05563fbedce6dceb31f0dc501835e56e1a9b9dd
Tim Weißenfels <timw-public@outlook.com>
2020-08-18 13:28:04 +0200

    Fixed skipping

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from radio.player import Player
 from radio.library import ClipLibrary
-from radio.audio.clips import Pause
-import time
 
 print("Radio starting...")
 library = ClipLibrary("test_library")
 player = Player(library)
 player.start()
-time.sleep(10.0)
-player.schedule(Pause(10))
-time.sleep(3.14)
-player.skip()
--- radio/audio/clips.py
+++ radio/audio/clips.py
@@ -3,6 +3,7 @@ import audio2numpy
 import sounddevice
 import os.path
 from threading import Event
+import time
 
 
 class Clip(ABC):
@@ -10,13 +11,16 @@ class Clip(ABC):
         self._aborted: bool = False
         self.user_req: bool = False
         self.name: str = name
+        self._abort = Event()
 
     @abstractmethod
     def start(self):
         pass
 
     def stop(self):
-        self._aborted = True
+        if not self._abort.is_set():
+            self._aborted = True
+            self._abort.set()
 
     def __str__(self):
         return self.name
@@ -31,12 +35,14 @@ class MP3Clip(Clip):
 
     def start(self):
         data, sr = audio2numpy.open_audio(self.file)
+        duration = len(data) / sr
         MP3Clip._dev.play(data, sr)
-        MP3Clip._dev.wait()
+        self._abort.wait(duration)
 
     def stop(self):
         MP3Clip._dev.stop()
         super().stop()
+        time.sleep(0.1)
 
 
 class Pause(Clip):
@@ -47,7 +53,3 @@ class Pause(Clip):
 
     def start(self):
         self._abort.wait(self.duration)
-
-    def stop(self):
-        if not self._abort.is_set():
-            self._abort.set()
