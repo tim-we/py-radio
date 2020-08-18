c88f8dfe263ffdc3fc63665a28840737e62206ed
Pinetrax <jannekwinter@gmail.com>
2020-08-18 23:07:36 +0200

    added history

--- bots/tg.py
+++ bots/tg.py
@@ -18,6 +18,8 @@ class Telegram:
         dispatcher.add_handler(exit_handler)
         skip_handler = CommandHandler('skip', self._skip)
         dispatcher.add_handler(skip_handler)
+        history_handler = CommandHandler('history', self._history)
+        dispatcher.add_handler(history_handler)
         unknown_handler = MessageHandler(Filters.command, self._unknown)
         dispatcher.add_handler(unknown_handler)
 
@@ -34,6 +36,13 @@ class Telegram:
             os._exit(int(context.args[0]))
         else:
             os._exit(0)
+
+    def _history(self, update: Any, context: Any) -> None:
+        history = '\n'.join(self._player.get_history())
+        if not history:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="The history is empty.")
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=history)
 
     def _skip(self, update: Any, context: Any) -> None:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Skipped "+self._player.now())
--- radio/player.py
+++ radio/player.py
@@ -4,15 +4,23 @@ from queue import Queue, LifoQueue
 from radio.audio.clips import Clip
 from radio.scheduler import Scheduler
 from radio.library import ClipLibrary
-from typing import Optional
+from typing import Optional, List
 
 
 class Player:
     def __init__(self, library: ClipLibrary):
         self._queue: Queue = LifoQueue()
         self._scheduler = Scheduler(library)
+        self._history = []
+        self._history_len = 7
         self._current: Optional[Clip] = None
         self._thread: Optional[Thread] = None
+
+    def get_history(self, num: int = 0) -> List[str]:
+        if num > 0:
+            return self._history[-min(num, self._history_len):]
+        else:
+            return self._history
 
     def now(self) -> str:
         if self._current is not None:
@@ -36,6 +44,11 @@ class Player:
             self._thread = Thread(target=self._play, daemon=False)
             self._thread.start()
 
+    def _add_to_history(self, name: str) -> None:
+        self._history.append(name)
+        if len(self._history) > self._history_len:
+            self._history.pop(0)
+
     def _play(self) -> None:
         while True:
             if self._queue.empty():
@@ -49,6 +62,7 @@ class Player:
             clip = self._queue.get()
             print("Playing", clip)
             self._current = clip
+            self._add_to_history(clip.name)
             try:
                 clip.start()
             except Exception as e:
