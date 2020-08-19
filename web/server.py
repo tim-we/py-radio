9adc4e259b4582134f9780a6bbd139b3c6534fa3
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2020-08-19 11:51:16 +0200

    improved web api + serve static page (#5)

--- web/server.py
+++ web/server.py
@@ -1,6 +1,6 @@
 from radio.player import Player
 from radio.library import ClipLibrary
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, send_from_directory
 from typing import Any
 from threading import Thread
 from waitress import serve
@@ -9,15 +9,47 @@ api_prefix = "/radio/api/v1.0"
 
 
 def create(player: Player, library: ClipLibrary, port: int = 5000) -> None:
-    flask = Flask("py-radio web-server")
+    flask = Flask(__name__.split('.')[0])
 
+    # -------------- WEBSITE --------------
+    @flask.route("/radio", methods=["GET"])
+    def web_main() -> Any:
+        return render_template("web.html")
+
+    @flask.route("/radio/static/<path:path>")
+    def web_static(path: Any) -> Any:
+        return send_from_directory("static", path)
+
+    # ---------------- API ----------------
     @flask.route(api_prefix + "/all", methods=["GET"])
     def api_now() -> Any:
         return jsonify({
             "status": "ok",
             "current": player.now(),
-            "history": player.get_history()
+            "history": player.get_history(),
+            "library": {
+                "music": library.music.size() + library.night.size(),
+                "hosts": library.hosts.size(),
+                "other": library.other.size()
+            }
         })
+
+    @flask.route(api_prefix + "/search/<string:search>", methods=["GET"])
+    def api_search(search: str) -> Any:
+        search = search.strip()[0:42]
+        if len(search) == 0:
+            return jsonify({
+                "status": "error",
+                "message": "Invalid search term."
+            })
+        else:
+            results = library.search_clips(search)
+            return jsonify({
+                "status": "ok",
+                "search": search,
+                "results": results[0:100],
+                "clipped": len(results) > 100
+            })
 
     @flask.route(api_prefix + "/skip", methods=["PUT"])
     def api_skip() -> Any:
@@ -25,6 +57,7 @@ def create(player: Player, library: ClipLibrary, port: int = 5000) -> None:
         return jsonify({"status": "ok"})
 
     def start() -> None:
+        print("API will be available at http://localhost:{}{}/".format(port, api_prefix))
         serve(
             flask,
             host="127.0.0.1",
