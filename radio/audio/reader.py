13f760d3c2a83e77233891b230d61b167894623a
Jannek Winter <Pinetrax@users.noreply.github.com>
2020-08-20 17:31:18 +0200

    Threading improvements (#16)
    * thread names
    
    * limit ffmpeg threads (?)
    
    * a little rest for the player
    
    * cpu_count() None fix
    
    * Clips now store duration
    
    * remove old code (unused)
    
    * make clip loaded event public
    
    * scheduler: wait for clip loaded
    
    * gave python scheduler a chance to interrupt while loading audio
    
    * get data from ffmpeg frame-wise to avoid copying a large amount of memory
    
    Co-authored-by: Tim Weißenfels <timw-public@outlook.com>

--- radio/audio/reader.py
+++ radio/audio/reader.py
@@ -1,36 +1,50 @@
 import numpy as np
 import ffmpeg
 import os
-from time import sleep
 
 
 # load_audio can not detect the input type
-def ffmpeg_load_audio(filename: str, mono: bool = False, normalize: bool = True) -> np.ndarray:  # type: ignore
-    channels = 1 if mono else 2
-    sample_rate = 48000
-    in_type = np.int16
-    num_bytes = 2*channels
-    out_type = np.float32
+def ffmpeg_load_audio(filename: str, normalize: bool = True) -> np.ndarray:  # type: ignore
     num_threads = max(1, (os.cpu_count() or 1)//2)
+    file_info = ffmpeg.probe(filename, threads=num_threads)
 
-    out, err = (ffmpeg
-                .input(filename, threads=num_threads)
-                .output('-', format='s16le', acodec='pcm_s16le', ar=str(sample_rate), ac=str(channels))
-                .overwrite_output()
-                .run(capture_stdout=True, quiet=True)
-                )
+    # get audio info
+    for stream_id, stream in enumerate(file_info['streams']):
+        if stream['codec_type'] == 'audio':
+            sample_rate = int(stream['sample_rate'])
+            channels = int(stream['channels'])
+            samples = int(float(stream['duration']) * sample_rate)
+            break
+    else:
+        return np.empty()
 
-    audio = np.zeros((len(out) // num_bytes, channels), out_type)
+    # start async decoding process to copy stdout frame-wise
+    decode = (ffmpeg
+              .input(filename, threads=num_threads)
+              .output('-', format='s16le', acodec='pcm_s16le', ar=str(sample_rate), ac=str(channels))
+              .overwrite_output()
+              .run_async(pipe_stdout=True, quiet=True)
+              )
+
+    # prepare output array and frame shift processing
     frame_shift = 1024
-    peak = 0 if normalize else np.iinfo(in_type).max
-    for i in range(audio.shape[0] // frame_shift):
-        audio[i*frame_shift:(i+1)*frame_shift, :] \
-            = np.frombuffer(out[i*num_bytes*frame_shift:(i+1)*num_bytes*frame_shift], dtype=in_type
-                            ).astype(out_type).reshape((-1, channels))
+    num_shifts = samples//frame_shift
+    audio = np.zeros((num_shifts*frame_shift, channels), dtype=np.float32)
+    num_bytes = 2*channels
+    # initialize peak for int->float conversion and normalizing
+    peak = 0 if normalize else np.iinfo(np.int16).max
+    for i in range(num_shifts):
+        in_bytes = decode.stdout.read(frame_shift*num_bytes)
+        if len(in_bytes) < frame_shift*num_bytes:
+            break
+        # convert bytes and shape into audio array if multiple channels
+        audio[i * frame_shift:(i + 1) * frame_shift, :] = np.frombuffer(in_bytes, dtype=np.int16)\
+            .astype(np.float32).reshape((-1, channels))
         if normalize:
             new_peak = np.max(audio[i*frame_shift:(i+1)*frame_shift+1, :])
             if peak < new_peak:
                 peak = new_peak
-        sleep(0)
+
+    # normalize (or convert from int range)
     audio = np.divide(audio, peak)
     return audio, sample_rate
