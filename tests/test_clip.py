1cc5a65ce2fd78c36fc488222c001be3bc5ee354
Tim Weißenfels <timw-public@outlook.com>
2020-08-24 20:54:41 +0200

    Remove test_clip.py

--- tests/test_clip.py
+++
@@ -1,9 +0,0 @@
-import sys
-from unittest import TestCase
-import mock
-sys.modules["radio.audio.ffmpeg_load_audio"] = mock.MagicMock()
-
-
-class TestClips(TestCase):
-    def test_this(self):
-        print(sys.modules.keys())
