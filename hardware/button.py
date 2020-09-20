96783cab060d5c2f60077865e1d3699f5b8e3193
Pinetrax <jannekwinter@gmail.com>
2020-09-20 19:53:56 +0200

    - wait now event-based to avoid busy-wait - implemented options for skip/pause

--- hardware/button.py
+++ hardware/button.py
@@ -1,24 +1,23 @@
 import radio.player
 from radio.audio import Pause
 from gpiozero import Button
+from threading import Thread, Event
 from time import time, sleep
 
 
 class RadioButton:
-    def _check(self):
-        start = time()
-        while self._button.is_active:
-            sleep(0.01)
-            if time() - start > 1:
-                break
-        else:
-            self._player.skip()
-            return
-
-        # if pressed long enough, initialize pause
-        if not isinstance(self._player.now(), Pause):
+    def _pressed(self):
+        self._event = Event()
+        self._event.wait(timeout=1)
+        if self._button.is_active and self._pause:
             self._player.schedule(Pause())
             self._player.skip()
+        else:
+            if self._skip:
+                self._player.skip()
+
+    def _released(self):
+        self._event.set()
 
     def __init__(
         self,
@@ -30,4 +29,6 @@ class RadioButton:
         self._player = player
         self._pause = pause
         self._skip = skip
-        self._button.when_activated = self._check
+        self._event = Event()
+        self._button.when_activated = self._pressed
+        self._button.when_deactivated = self._released
