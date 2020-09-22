be59cd2e9c8c6e3e1864f532ebad500ab1c218ff
Pinetrax <jannekwinter@gmail.com>
2020-09-22 16:04:56 +0200

    fix mypy

--- hardware/button.py
+++ hardware/button.py
@@ -6,22 +6,6 @@ from time import time as now
 
 
 class RadioButton:
-    def _pressed(self):
-        if self._time_limit > now():
-            return
-        self._event = Event()
-        self._event.wait(timeout=1)
-        if self._button.is_active and self._pause:
-            self._player.schedule(Pause())
-            self._player.skip()
-        else:
-            if self._skip:
-                self._player.skip()
-
-    def _released(self):
-        self._time_limit = now() + 0.5
-        self._event.set()
-
     def __init__(
         self,
         player: 'radio.player.Player',
@@ -33,6 +17,22 @@ class RadioButton:
         self._pause = pause
         self._skip = skip
         self._event = Event()
-        self._time_limit = 0
+        self._time_limit: float = 0
         self._button.when_activated = self._pressed
         self._button.when_deactivated = self._released
+
+    def _pressed(self) -> None:
+        if self._time_limit > now():
+            return
+        self._event = Event()
+        self._event.wait(timeout=1)
+        if self._button.is_active and self._pause:
+            self._player.schedule(Pause())
+            self._player.skip()
+        else:
+            if self._skip:
+                self._player.skip()
+
+    def _released(self) -> None:
+        self._time_limit = now() + 0.5
+        self._event.set()
