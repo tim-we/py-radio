b91370f742d7ef09f39749f877d4efe0f4801d09
Tim Weißenfels <tim-we@users.noreply.github.com>
2020-12-12 18:06:31 +0100

    Potential button fix

--- hardware/button.py
+++ hardware/button.py
@@ -28,31 +28,29 @@ class RadioButton:
         if self._time_limit > now():
             return
 
-        def button_press_handler() -> None:
-            # reset event (unset)
-            self._event.clear()
-
-            if self._pause:
+        def button_press_handler(button: RadioButton) -> None:
+            if button._pause:
                 # schedule pause to avoid further playback
-                self._player.schedule(Pause())
+                button._player.schedule(Pause())
 
             # skip immediately (instant feedback)
-            self._player.skip()
+            button._player.skip()
 
-            if self._pause and self._skip:
+            if button._pause and self._skip:
                 # wait for button to be released or 1 second to pass
                 # wait(timeout=1) returns True iff the event is set, not when it timed out
-                x = self._event.wait(timeout=1)
-                if x:
+                if button._event.wait(timeout=1):
                     # the button was released within 1 second of being pressed
                     # assume the user just wanted to skip the current song
                     # thus skip the current pause
-                    self._player.skip()
+                    button._player.skip()
 
-            self._event.clear()
+            button._event.clear()
 
+        # reset event (unset)
+        self._event.clear()
         # avoid thread blocking (otherwise _released() would wait for _pressed() to finish)
-        Thread(target=button_press_handler).start()
+        Thread(target=button_press_handler, args=(self, )).start()
 
     def _released(self) -> None:
         self._time_limit = now() + 0.5
